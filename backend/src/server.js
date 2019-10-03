989b18407c3912cfce2900d1750e7e94c5a6935a
fbc623 <fbc623@gmail.com>
2019-10-03 00:01:57 -0300

    cirado o fontend web

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose  = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
 
 const routes = require('./routes');
 
@@ -17,7 +20,10 @@ mongoose.connect('mongodb+srv://omnistack:88281783f@cluster0-kksmh.mongodb.net/s
 //reqparams = acessar route params ( para edicao e delete)
 //req.body = acessar corpo da rqueisicao (para criacao e edicao)
 
+
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname,'..','uploads' )));
 app.use(routes);
 
 app.listen(3333);
